3578e84a5acd7eb877dad5bfbfebca782787e85f
ezhamkarim <ezhamezzuddin@gmail.com>
2022-06-12 17:16:35 +0800

    setup app

---
+++ lib/src/router_app.dart
@@ -0,0 +1,18 @@
+class RouterApp {
+  // static Route onGenerateRoute(RouteSettings routeSettings) {
+  //   return MaterialPageRoute<void>(
+  //     settings: routeSettings,
+  //     builder: (BuildContext context) {
+  //       switch (routeSettings.name) {
+  //         case SettingsView.routeName:
+  //           return SettingsView(controller: settingsController);
+  //         case SampleItemDetailsView.routeName:
+  //           return const SampleItemDetailsView();
+  //         case SampleItemListView.routeName:
+  //         default:
+  //           return const SampleItemListView();
+  //       }
+  //     },
+  //   );
+  // }
+}
