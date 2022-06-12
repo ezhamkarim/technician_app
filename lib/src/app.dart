aa0383c1e534722b8d51918ae60c05d4da1afe30
ezhamkarim <ezhamezzuddin@gmail.com>
2022-06-13 01:25:57 +0800

    add provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:technician_app/src/provider/root_provider.dart';
 import 'package:technician_app/src/router_app.dart';
+import 'package:technician_app/src/services/auth_services.dart';
 import 'package:technician_app/src/style/custom_style.dart';
 
 import 'settings/settings_controller.dart';
@@ -25,46 +29,58 @@ class MyApp extends StatelessWidget {
     return AnimatedBuilder(
       animation: settingsController,
       builder: (BuildContext context, Widget? child) {
-        return MaterialApp(
-          // Providing a restorationScopeId allows the Navigator built by the
-          // MaterialApp to restore the navigation stack when a user leaves and
-          // returns to the app after it has been killed while running in the
-          // background.
-          restorationScopeId: 'app',
-
-          // Provide the generated AppLocalizations to the MaterialApp. This
-          // allows descendant Widgets to display the correct translations
-          // depending on the user's locale.
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider<RootProvider>(create: (_) => RootProvider()),
+            Provider(
+              create: (_) => AuthService(FirebaseAuth.instance),
+            ),
+            StreamProvider(
+                create: (context) =>
+                    context.read<AuthService>().authStateChanges,
+                initialData: null)
           ],
-          supportedLocales: const [
-            Locale('en', ''), // English, no country code
-          ],
+          child: MaterialApp(
+            // Providing a restorationScopeId allows the Navigator built by the
+            // MaterialApp to restore the navigation stack when a user leaves and
+            // returns to the app after it has been killed while running in the
+            // background.
+            restorationScopeId: 'app',
 
-          // Use AppLocalizations to configure the correct application title
-          // depending on the user's locale.
-          //
-          // The appTitle is defined in .arb files found in the localization
-          // directory.
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
+            // Provide the generated AppLocalizations to the MaterialApp. This
+            // allows descendant Widgets to display the correct translations
+            // depending on the user's locale.
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('en', ''), // English, no country code
+            ],
 
-          // Define a light and dark color theme. Then, read the user's
-          // preferred ThemeMode (light, dark, or system default) from the
-          // SettingsController to display the correct theme.
-          theme: ThemeData(
-              primaryColor: CustomStyle.primarycolor,
-              fontFamily: GoogleFonts.inter().fontFamily),
-          darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
+            // Use AppLocalizations to configure the correct application title
+            // depending on the user's locale.
+            //
+            // The appTitle is defined in .arb files found in the localization
+            // directory.
+            onGenerateTitle: (BuildContext context) =>
+                AppLocalizations.of(context)!.appTitle,
 
-          // Define a function to handle named routes in order to support
-          // Flutter web url navigation and deep linking.
-          onGenerateRoute: RouterApp.onGenerateRoute,
+            // Define a light and dark color theme. Then, read the user's
+            // preferred ThemeMode (light, dark, or system default) from the
+            // SettingsController to display the correct theme.
+            theme: ThemeData(
+                primaryColor: CustomStyle.primarycolor,
+                fontFamily: GoogleFonts.inter().fontFamily),
+            darkTheme: ThemeData.dark(),
+            themeMode: settingsController.themeMode,
+
+            // Define a function to handle named routes in order to support
+            // Flutter web url navigation and deep linking.
+            onGenerateRoute: RouterApp.onGenerateRoute,
+          ),
         );
       },
     );
