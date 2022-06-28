86a613b6dc5b078960ecbc7bc4695c15fb78670d
ezhamkarim <ezhamezzuddin@gmail.com>
2022-06-28 23:59:08 +0800

    dd

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -64,6 +64,7 @@ class _MyAppState extends State<MyApp> {
                 initialData: null)
           ],
           child: MaterialApp(
+            debugShowCheckedModeBanner: false,
             // Providing a restorationScopeId allows the Navigator built by the
             // MaterialApp to restore the navigation stack when a user leaves and
             // returns to the app after it has been killed while running in the
